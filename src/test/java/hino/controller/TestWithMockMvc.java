96846e3d01ba5a88285cd0557d42e63c0a8537d5
hino_hatake <sinhngay3110@gmail.com>
2019-07-23 01:01:48 +0700

    exclude JUnit4

--- src/test/java/hino/controller/TestWithMockMvc.java
+++ src/test/java/hino/controller/TestWithMockMvc.java
@@ -3,13 +3,11 @@ package hino.controller;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
 public class TestWithMockMvc {
 
   @Autowired
--- src/test/java/hino/controller/TestWithRestTemplate.java
+++ src/test/java/hino/controller/TestWithRestTemplate.java
@@ -13,7 +13,7 @@
  */
 package hino.controller;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
