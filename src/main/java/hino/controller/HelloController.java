00f509094ab779a418f53f8c2d118b167574249c
ntquan <ntquan@cmc.com.vn>
2019-07-23 14:24:47 +0700

    add actuator

--- src/main/java/hino/controller/HelloController.java
+++ src/main/java/hino/controller/HelloController.java
@@ -2,6 +2,7 @@ package hino.controller;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
   @RequestMapping("/")
-  public String index() throws IOException {
-    return "Greetings from Hino!<br>Here is your Inet Address: " + InetAddress.getLocalHost();
+  public ResponseEntity<String> index() throws IOException {
+    return ResponseEntity.ok("Greetings from Hino!<br>Here is your Inet Address: " + InetAddress.getLocalHost());
   }
 }
