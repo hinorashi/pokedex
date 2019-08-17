927afb9c40fad82ef59d7e6cb2d40bdedad48a98
hino_hatake <sinhngay3110@gmail.com>
2019-08-17 16:18:45 +0700

    add some stupd message for /hello endpoint

--- src/main/java/hino/controller/HelloController.java
+++ src/main/java/hino/controller/HelloController.java
@@ -2,7 +2,8 @@ package hino.controller;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import org.springframework.http.MediaType;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,16 @@ import io.swagger.annotations.ApiOperation;
 
 @Api(tags = "hello", description = "Say hello", protocols = "http")
 @RestController
-@RequestMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+@RequestMapping(path = "/")
 public class HelloController {
 
   @ApiOperation(value = "This is how we say hello in Zaun!", notes = "Nothing to note...", response = String.class)
   @GetMapping
   public ResponseEntity<String> index() throws IOException {
-    return ResponseEntity.ok("Greetings from Hino! Here is your Inet Address: " + InetAddress.getLocalHost());
+    return ResponseEntity.ok("Greetings from Hino! "
+        + "<br>Here is your Inet Address: " + InetAddress.getLocalHost()
+        + "<br>I think you should check the time: <span style=\"color:red\">"
+        + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"))
+        + "</span>");
   }
 }
