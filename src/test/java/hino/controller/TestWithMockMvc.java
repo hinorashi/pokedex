d59e6863d83cd08b4358c88884d3f46481e0c11f
hino_hatake <sinhngay3110@gmail.com>
2020-06-08 00:59:18 +0700

    add @AutoConfigureDataJpa to be used with @WebMvcTest

--- src/test/java/hino/controller/TestWithMockMvc.java
+++ src/test/java/hino/controller/TestWithMockMvc.java
@@ -3,11 +3,16 @@ package hino.controller;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest
+@AutoConfigureDataJpa
 public class TestWithMockMvc {
 
   @Autowired
